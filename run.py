986c22c3ab71428eba3486da28660adfa02a9662
enock <enckkipronoh@gmail.com>
2021-09-03 22:42:09 +0300

    adding figlet

--- run.py
+++ run.py
@@ -1,2 +1,30 @@
 #!/usr/bin/env python3.8
 
+from user import User
+def function():
+	print("               ____                             ")
+	print("              |  _ \                            ")
+	print("              | |_) )  ____  ___   ___          ")
+	print("              |  __/  / _  |/ __  / __          ")
+	print("              | |    / (_| |\__ \ \__ \         ")
+	print("              |_|    \_____| ___/  ___/         ")
+function()
+
+def create_user(user_name,first_name,password):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(user_name,first_name,password)
+    return new_user
+
+def save_users(user):
+    '''
+    Function to save user
+    '''
+    user.save_user()
+
+def del_user(contact):
+    '''
+    Function to delete a contact
+    '''
+    contact.delete_contact()
