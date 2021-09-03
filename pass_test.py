30a525377a15435570d02ab724cc655e0eb3df74
enock <enckkipronoh@gmail.com>
2021-09-03 17:15:43 +0300

    testing for user existence done

--- pass_test.py
+++ pass_test.py
@@ -88,6 +88,13 @@ class TestContact(unittest.TestCase):
         self.assertTrue(user_exists)
 
         
+
+    def test_display_all_users(self):
+        '''
+        method that returns a list of all users saved
+        '''
+
+        self.assertEqual(User.display_users(),User.user_list)
         
 
 
