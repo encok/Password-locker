8350922fc21363b20df7e70ffde59a2ea647ff97
enock <enckkipronoh@gmail.com>
2021-09-03 15:28:41 +0300

    creating the instance variables

--- user.py
+++ user.py
@@ -1,21 +1,23 @@
 print("...........................Password Locker...........................")
 class User:
     """
-    Class that generates new instances of contacts
+    Class that generates new instances of contacts.
     """
 
-    user_list = []
-    
-    def__init__(self, username, first_name, password):
-        """
+    user_list = [] # Empty contact list
+
+    def __init__(self,user_name,first_name,password):
+
+        '''
         __init__ method that helps us define properties for our objects.
 
         Args:
-            username: New user usernaname.
-            first_name : New user first name.
-            password: New user password.
-            
-        """
-        self.username = username
+            first_name: New contact first name.
+            last_name : New contact last name.
+            number: New contact phone number.
+            email : New contact email address.
+        '''
+        
+        self.user_name = user_name
         self.first_name = first_name
         self.password = password
