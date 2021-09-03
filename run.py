76bb03c75b5c6a83e3fd1de6e5e3eb2f9c8a699a
enock <enckkipronoh@gmail.com>
2021-09-03 22:46:17 +0300

    finding contact form run file

--- run.py
+++ run.py
@@ -23,8 +23,20 @@ def save_users(user):
     '''
     user.save_user()
 
-def del_user(contact):
+def del_user(user):
     '''
-    Function to delete a contact
+    Function to delete a user
     '''
-    contact.delete_contact()
+    user.delete_user()
+
+def find_user(password):
+    '''
+    Function that finds a user by password and returns the user
+    '''
+    return User.find_by_password(password)
+
+def check_existing_users(password):
+    '''
+    Function that check if a user exists with that password and return a Boolean
+    '''
+    return User.user_exist(password)
